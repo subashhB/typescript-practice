4ae26ac0bf9b54dcc8dcc469fad8bd28b3cd234e
Admin <subashbaidya9@gmail.com>
2023-04-06 18:40:35 +0545

    Compiling TypeScript

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = 'Hero Babu';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
